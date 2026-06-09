a0ad4c86a959f91283fd862680d0ada23dcd6d8f
comsize-hatakeyama <takumi.hatakeyama@comsize.com>
2026-06-09 14:14:43 +0900

    衝突時に起きた変更の削除

--- src/main/java/model/dao/ConnectionManager.java
+++ src/main/java/model/dao/ConnectionManager.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-<<<<<<< HEAD
 	private final static String URL = "jdbc:mysql://localhost:3306/task_db?useSSL=false";
 	private final static String USER = "root";
 	private final static String PASSWORD = "root";
@@ -15,22 +14,4 @@ public class ConnectionManager {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		return DriverManager.getConnection(URL,USER,PASSWORD);
 	}
-=======
-
-	//DB接続用のurl
-	private final static String URL = "jdbc:mysql://localhost:3306/task_db";
-	private final static String USER = "root";
-	private final static String PASSWORD = "root";
-
-	//メソッドの定義
-	public static Connection getConnection() throws SQLException, ClassNotFoundException {
-
-		//JDBCの読み取り
-		Class.forName("com.mysql.cj.jdbc.Driver");
-
-		return DriverManager.getConnection(URL, USER, PASSWORD);
-
-	}
-
->>>>>>> 3052bb8b5f3206518911ac56fb1c072fdd4160f3
 }
